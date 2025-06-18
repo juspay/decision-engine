b87bc1a82976e0b07d0f4c4d44526042aac67c7f
prajjwalkumar17 <prajjwal.kumar@juspay.in>
2025-06-19 01:05:03 +0530

    test fmt

--- src/euclid/ast.rs
+++ src/euclid/ast.rs
@@ -26,7 +26,8 @@ pub enum ValueType {
     /// Represents an array of numbers. This is basically used for
     /// "one of the given numbers" operations
     /// eg: payment.method.amount = (1, 2, 3)
-    NumberArray(Vec<u64>),
+    NumberArray(
+        Vec<u64>),
     /// Similar to NumberArray but for enum variants
     /// eg: payment.method.cardtype = (debit, credit)
     EnumVariantArray(Vec<String>),
