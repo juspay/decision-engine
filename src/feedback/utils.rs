4c003c081ddd65566e850793391ba98256f8db13
Jagan Elavarasan <jaganelavarasan@gmail.com>
2025-07-29 12:28:54 +0530

    fix formatting

--- src/feedback/utils.rs
+++ src/feedback/utils.rs
@@ -206,7 +206,7 @@ pub fn getTxnDetailFromApiPayload(
         internalTrackingInfo: None,
         partitionKey: None,
         txnAmountBreakup: None,
-        txnLatency: apiPayload.txnLatency
+        txnLatency: apiPayload.txnLatency,
     };
     txn_detail
 }
